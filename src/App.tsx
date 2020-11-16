3fe4d2175b96f45845f1c2181b9fd5cbd9ab2525
Zachary Graham <zakgraham@gmail.com>
2020-11-16 13:10:18 -0700

    create main components

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+//components
+import Header from "./component/Header/Header";
+import IssuesBar from "./component/IssuesBar/IssuesBar";
+import CurrentDiscussion from "./component/CurrentDiscussion/CurrentDiscussion";
+import Footer from "./component/Footer/Footer";
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      zg-debates
+      <Header />
+      <IssuesBar />
+      <CurrentDiscussion />
+      <Footer />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/component/CurrentDiscussion/CurrentDiscussion.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CurrentDiscussion: React.FC = () => {
+  return <div className="zg-current-discussion">CurrentDiscussion</div>;
+};
+
+export default CurrentDiscussion;
