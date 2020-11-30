13fc4831e6682c95db4a7c0c7d8a9bd77f68100c
Zachary Graham <zakgraham@gmail.com>
2020-11-30 16:07:34 -0700

    add login component and router and types for router

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import Header from "./component/Header/Header";
 import IssuesBar from "./component/CategoriesBar/CategoriesBar";
 import CurrentDiscussion from "./component/CurrentDiscussion/CurrentDiscussion";
 import Footer from "./component/Footer/Footer";
+import Login from "./component/Login/Login";
 
 const App: React.FC = () => {
   return (
@@ -13,6 +14,7 @@ const App: React.FC = () => {
       <Header />
       <IssuesBar />
       <CurrentDiscussion />
+      <Login/>
       <Footer />
     </div>
   );
--- src/component/Login/Login.tsx
+++ src/component/Login/Login.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+
 import "./Login.scss";
 
 const Login: React.FC = () => {
