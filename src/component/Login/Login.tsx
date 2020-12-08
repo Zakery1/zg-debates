0e9d7bed4eaa41c9d645469b37f2abf6c90a3cdb
Zachary Graham <zakgraham@gmail.com>
2020-12-07 23:46:56 -0700

    add login inputs and buttons

--- src/component/Login/Login.tsx
+++ src/component/Login/Login.tsx
@@ -1,20 +1,21 @@
 import React from "react";
 
 
-
 import "./Login.scss";
+import { Link } from "react-router-dom";
 
 const Login: React.FC = () => {
   return (
     <div className="zg-login">
-      
-      <div className="zg-login-title">
-        New Twitter
-      </div>
+      <div className="zg-login-title">New Twitter</div>
       <div className="zg-login-portal">
         Login portal
+        <input type="text" placeholder="Email or Username" />
+        <input type="password" placeholder="Password" />
+        <button>Log In</button>
+        <Link to="/login">Forgot PLinkssword?</Link>
+        <button>Sign Up</button>
       </div>
-
     </div>
   );
 };
