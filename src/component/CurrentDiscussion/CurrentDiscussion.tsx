4fda8ccf544e641e7b166f23c537ee124507aa3e
Zachary Graham <zakgraham@gmail.com>
2020-12-07 23:20:17 -0700

    add basic structure styles to current discussion

--- src/component/CurrentDiscussion/CurrentDiscussion.tsx
+++ src/component/CurrentDiscussion/CurrentDiscussion.tsx
@@ -1,13 +1,25 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import "./CurrentDiscussion.scss";
+
 const CurrentDiscussion: React.FC = () => {
   return (
     <div className="zg-current-discussion">
       <br />
-      CurrentDiscussion: <br />
-      We will have the discussion on this page 
       <br />
+
+      <h3>Discusion title/argument will go here.</h3>
+      <br />
+      <div className="zg-position-container">
+        <div className="zg-agree">Agree</div>
+        <div className="zg-neutral">Neutral</div>
+        <div className="zg-disagree">Disagree</div>
+      </div>
+
+      <br />
+
+      <Link to="/category">Back to Premises</Link>
       <br />
       <Link to="/">Home</Link>
     </div>
