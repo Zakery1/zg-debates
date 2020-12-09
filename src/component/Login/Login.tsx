628227b967c22b3fac879d91bb74ba9bfd708289
Zachary Graham <zakgraham@gmail.com>
2020-12-08 23:03:18 -0700

    add responsiveness to mobile login

--- src/component/Login/Login.tsx
+++ src/component/Login/Login.tsx
@@ -1,19 +1,19 @@
 import React from "react";
 
-
 import "./Login.scss";
 import { Link } from "react-router-dom";
 
 const Login: React.FC = () => {
   return (
     <div className="zg-login">
-      <div className="zg-login-title">New Twitter</div>
+      <div className="zg-login-title">
+        <h1>New Twitter</h1>
+      </div>
       <div className="zg-login-portal">
-        Login portal
-        <input type="text" placeholder="Email or Username" />
-        <input type="password" placeholder="Password" />
-        <button>Log In</button>
-        <Link to="/login">Forgot PLinkssword?</Link>
+        <input className="zg-login-username" type="text" placeholder="Email or Username" />
+        <input className="zg-login-password" type="password" placeholder="Password" />
+        <button className="zg-login-button">Log In</button>
+        <Link to="/login">Forgot Password?</Link>
         <button>Sign Up</button>
       </div>
     </div>
