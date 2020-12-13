413cd075caf16692e8b9a93e187cba97eb993bf5
Zachary Graham <zakgraham@gmail.com>
2020-12-12 21:28:50 -0700

    add min height to main components

--- src/component/Category/Category.tsx
+++ src/component/Category/Category.tsx
@@ -2,25 +2,24 @@ import React from "react";
 
 import { Link } from "react-router-dom";
 
+import "./Category.scss";
+
 // interface Category {
 //   id: number;
 //   name: string;
 // }
 
-
-
 //get Categories that match category
 
 const Category: React.FC = () => {
   return (
-    <div className="App">
-      Category Component
+    <div className="zg-category">
       <br />
       <Link to="/category/issue">Orange Man Bad</Link>
       <br />
       {/* <Link to={{pathname: issue}}>Another premise</Link> */}
       <Link to="/category/issue">Orange Man Good</Link>
-      <br/>
+      <br />
       <br />
       <br />
     </div>
