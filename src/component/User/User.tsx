ed3c877a45e7a75e81995c317a5f30d12c38d607
Zachary Graham <zakgraham@gmail.com>
2020-12-16 22:53:20 -0700

    comment out axios get user request

--- src/component/User/User.tsx
+++ src/component/User/User.tsx
@@ -1,21 +1,21 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 
-import axios from "axios";
+// import axios from "axios";
 
 const User: React.FC = () => {
-  const [username, setUsername] = useState<string>("");
+  // const [username, setUsername] = useState<string>("");
 
-  useEffect(() => {
-    async function fetchUser() {
-      await axios
-        .get(`https://fathomless-reaches-38159.herokuapp.com/api/getUserById/1`)
-        .then((res) => {
-          const user = res.data;
-          setUsername(user);
-        });
-    }
-    fetchUser();
-  }, [username]);
+  // useEffect(() => {
+  //   async function fetchUser() {
+  //     await axios
+  //       .get(`https://fathomless-reaches-38159.herokuapp.com/api/getUserById/1`)
+  //       .then((res) => {
+  //         const user = res.data;
+  //         setUsername(user);
+  //       });
+  //   }
+  //   fetchUser();
+  // }, [username]);
 
   return <div></div>;
 };
