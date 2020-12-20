b6d8ebc96d02fe64892e2a12a6abcf3ad38d3851
Zachary Graham <zakgraham@gmail.com>
2020-12-19 22:53:01 -0700

    add coniditional rendering to choices

--- src/component/Contribution/Contribution.tsx
+++ src/component/Contribution/Contribution.tsx
@@ -5,8 +5,8 @@ import Modal from "@material-ui/core/Modal";
 import "./Contribution.scss";
 
 const Contribution: React.FC = () => {
-  // getModalStyle is not a pure function, we roll the style only on the first render
   const [open, setOpen] = useState(false);
+  const [choice, setChoice] = useState("");
 
   const handleOpen = () => {
     setOpen(true);
@@ -19,13 +19,19 @@ const Contribution: React.FC = () => {
   const body = (
     <div className="zg-body">
       <h2>Make a contribution to the discussion ---- discussion name</h2>
-      <p></p>
-      <textarea className="zg-contribution-input" autoFocus/>
-      <br/>
-      <br/>
+      <div className="zg-choice-group">
+
+        <button onClick={() => setChoice("agree")} className={"zg-choice-agree " + (choice === "agree" ? "zg-choice-selected" : "" )}>I agree</button>
+        <button onClick={() => setChoice("neutral")} className={"zg-choice-neutral " + (choice === "neutral" ? "zg-choice-selected" : "" )}>I'm neutral</button>
+        <button onClick={() => setChoice("disagree")} className={"zg-choice-disagree " + (choice === "disagree" ? "zg-choice-selected" : "" )}>I disagree</button>
+      </div>
+
+      <textarea className="zg-contribution-input" autoFocus />
+      <br />
+      <br />
       <button className="zg-submit-contribution">Submit contribution</button>
-      <br/>
-      <br/>
+      <br />
+      <br />
       <button onClick={handleClose}>Cancel</button>
     </div>
   );
