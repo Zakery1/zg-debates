d6d9bd1053255f68b06950bd26d9d60f4419f1ed
Zachary Graham <zakgraham@gmail.com>
2021-01-02 18:09:07 -0700

    add axios calls for voting

--- src/component/Vote/Vote.tsx
+++ src/component/Vote/Vote.tsx
@@ -16,6 +16,10 @@ interface VoteProps {
 const Vote: React.FC<VoteProps> = (props: VoteProps) => {
   const [voted, setVoted] = useState<boolean>(props.initialVote);
 
+  let userId: number = 1;
+
+  let deleteConfig = { userId: userId, contributionId: props.contributionId}
+
   let removeVote = async () => {
     await axios
       .put(`http://localhost:3000/api/subtractPointFromContribution`, {
@@ -25,14 +29,23 @@ const Vote: React.FC<VoteProps> = (props: VoteProps) => {
         console.log(res.status);
       });
 
-    // setUpdatedContribution("");
-    // handleClose();
-    // window.location.reload();
+    await axios
+      .delete(`http://localhost:3000/api/removeVoteFromRecord`, {data: deleteConfig}).then((res) => {
+        console.log(res.status);
+      });
   };
 
   let addVote = async () => {
     await axios
       .put(`http://localhost:3000/api/addPointToContribution`, {
+        contributionId: props.contributionId,
+      })
+      .then((res) => {
+        console.log(res.status);
+      });
+    await axios
+      .post(`http://localhost:3000/api/addVoteToRecord`, {
+        userId: 1,
         contributionId: props.contributionId,
       })
       .then((res) => {
@@ -46,7 +59,6 @@ const Vote: React.FC<VoteProps> = (props: VoteProps) => {
       removeVote();
     } else {
       addVote();
-      //add vote, addPointToContribution and addVoteToRecord
     }
   };
 
