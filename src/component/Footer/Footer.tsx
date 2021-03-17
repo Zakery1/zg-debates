703ff99873b64c0e88f6999b17cb57eaad4f798b
Zachary Graham <zakgraham@gmail.com>
2021-03-17 14:39:31 -0700

    update footer date

--- src/component/Footer/Footer.tsx
+++ src/component/Footer/Footer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./Footer.scss";
 
 const Footer: React.FC = () => {
-  return <div className="zg-footer">New Twitter, 2020</div>;
+  return <div className="zg-footer">New Twitter, 2021</div>;
 };
 
 export default Footer;
