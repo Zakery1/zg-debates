13b9fbde82051f73b60d15baae3516ccaaa2aee1
Zachary Graham <zakgraham@gmail.com>
2021-03-21 17:02:04 -0700

    create suggestions component and add suggestions route

--- src/component/Welcome/Welcome.tsx
+++ src/component/Welcome/Welcome.tsx
@@ -7,9 +7,14 @@ const Welcome: React.FC = () => {
   return (
     <div className="zg-welcome">
       <h1 className="zg-welcome-header">Welcome</h1>
-      <p>Welcome to ZG-Debates.  Our goal is to create better quality<br/> discourse surrounding important and challenging topics. </p>
-      <p>If you feel compelled, feel free to give suggestions on how we can improve <Link to="./suggestions">here</Link>.</p>
-
+      <p>
+        Welcome to ZG-Debates. Our goal is to create better quality
+        <br /> discourse surrounding important and challenging topics.{" "}
+      </p>
+      <p>
+        If you feel compelled, feel free to give suggestions on how we can
+        improve <Link to="/suggestions">here</Link>.
+      </p>
     </div>
   );
 };
