e43833572c7dd4d42a155d12c50bd084f2c0987c
Zachary Graham <zakgraham@gmail.com>
2021-03-22 23:01:15 -0700

    put create suggestion in seperate compoent

---
+++ src/component/CreateSuggestion/CreateSuggestion.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+import "./CreateSuggestion.scss";
+
+const CreateSuggestion: React.FC = () => {
+
+  //need new suggestions table
+    //retreive existing suggestions
+    //need to submit suggestions to new suggestions table
+    //only allow admin to delete
+
+
+
+  return (
+    <div className="zg-create-suggestion">
+      <h1>Suggestions</h1>
+      Suggestion:
+      <input type="text" />
+      <br />
+      Suggestion author:
+      <input type="text" />
+      <br/>
+      <button>submit suggestion</button>
+      <div>
+          <h3>Existing Suggestions</h3>
+      </div>
+    </div>
+  );
+};
+
+export default CreateSuggestion;
--- src/component/Suggestions/Suggestions.tsx
+++ src/component/Suggestions/Suggestions.tsx
@@ -1,31 +1,16 @@
 import React from "react";
 
+import CreateSuggestion from "../CreateSuggestion/CreateSuggestion";
+
 import "./Suggestions.scss";
 
 const Suggestions: React.FC = () => {
-
   //need new suggestions table
-    //retreive existing suggestions
-    //need to submit suggestions to new suggestions table
-    //only allow admin to delete
+  //retreive existing suggestions
+  //need to submit suggestions to new suggestions table
+  //only allow admin to delete
 
-
-
-  return (
-    <div className="zg-suggestions">
-      <h1>Suggestions</h1>
-      Suggestion:
-      <input type="text" />
-      <br />
-      Suggestion author:
-      <input type="text" />
-      <br/>
-      <button>submit suggestion</button>
-      <div>
-          <h3>Existing Suggestions</h3>
-      </div>
-    </div>
-  );
+  return <div className="zg-suggestions"><CreateSuggestion/></div>;
 };
 
 export default Suggestions;
