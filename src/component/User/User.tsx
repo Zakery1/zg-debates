6b47d71fda04bf0aaca85d492e15e2fe873af1dd
Zachary Graham <zakgraham@gmail.com>
2021-05-11 23:25:28 -0700

    add fetch user's contributions and add basic info to users page

--- src/component/User/User.tsx
+++ src/component/User/User.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState, useEffect } from "react";
 
 import { Link } from "react-router-dom";
 
@@ -8,11 +8,55 @@ import { SimpleCtx } from "../../context/UserContext";
 
 import "./User.scss";
 
-const User: React.FC = () => {
-  const value = useContext(SimpleCtx);
+interface ContributionItem {
+  id: number | null;
+  userId: number | null;
+  discussionId: number | null;
+  contribution: string;
+  agree: boolean | null;
+  neutral: boolean | null;
+  disagree: boolean | null;
+  points: number;
+  contributionDate: string | null;
+  hyperboles: number;
+  trolls: number;
+}
 
+interface ContributionsArray extends Array<ContributionItem> {}
+
+const User: React.FC = () => {
   const baseUrl =
     process.env.REACT_APP_SERVER_URL || process.env.REACT_APP_LOCAL_SERVER;
+
+  const [
+    userContributions,
+    setUserContributions,
+  ] = useState<ContributionsArray>([
+    {
+      id: null,
+      userId: null,
+      discussionId: null,
+      contribution: "",
+      agree: null,
+      neutral: null,
+      disagree: null,
+      points: 0,
+      contributionDate: null,
+      hyperboles: 0,
+      trolls: 0,
+    },
+  ]);
+
+  const value = useContext(SimpleCtx);
+
+  const getUserContributions = async () => {
+    await axios
+      .get(`${baseUrl}/api/contributions/?userId=${value?.id}`)
+      .then((res) => {
+        // console.log("CONTRIBUTION RESOURCE", res.data)
+        setUserContributions(res.data);
+      });
+  };
 
   const logout = async () => {
     await axios.post(`${baseUrl}/api/users/logout`).then((response) => {
@@ -22,10 +66,42 @@ const User: React.FC = () => {
     });
   };
 
+  useEffect(() => {
+    const getUserContributions = async () => {
+      await axios
+        .get(`${baseUrl}/api/contributions/?userId=${value?.id}`)
+        .then((res) => {
+          setUserContributions(res.data);
+        });
+    };
+    getUserContributions();
+  }, []);
+
+  let contributionList = () =>
+    userContributions.map((contribution) => {
+      return (
+        <span key={contribution.id}>
+          <div>
+            {contribution.contribution}
+            {contribution.points}
+            {contribution.hyperboles}
+            {contribution.trolls}
+            <button>delete</button>
+          </div>
+          <br />
+          <Link to={`/discussion/${contribution.discussionId}`}>
+            Go to discussion
+          </Link>
+        </span>
+      );
+    });
+
   return (
     <div>
       User
       <div>
+        {contributionList()}
+        <br />
         <Link className="zg-logout" to="/" onClick={logout}>
           Logout
         </Link>
